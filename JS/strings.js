758f44f36afe709a4306c8b684a7664f75184640
kilobyte-developer <atharvafunde26@gmail.com>
2024-07-30 20:15:35 +0530

    DOM
    Started With DOM

---
+++ JS/strings.js
@@ -0,0 +1,13 @@
+//Tutorial on Strings Video 60
+
+let a = "Atharva"
+
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(a[5])
+console.log(a[6])
+
+console.log(a.length)
